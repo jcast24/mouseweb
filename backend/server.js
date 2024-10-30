8073e75bb84593a3ea2e91101fffb867b37e8aa8
Jesus Castro <castrojesus39@gmail.com>
2024-10-29 20:25:41 -0700

    middleware for cors and body-parser has been added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { DB } from "./connect.js";
 
+// express stuff
 const app = express();
 const PORT = 3000;
+
+// cors stuff
+const corsOptions = {
+    origin:["http://localhost:3000"],
+}
+app.use(cors(corsOptions));
+
+// bodyParser.json() converts the json string into a JS object
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Everything is working!");
+})
+
 app.listen(PORT, (err) => {
     if (err) {
         console.error(err.message);
