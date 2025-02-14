008f689a0c69474e4d28c4ea0e99ba8c57248859
Jesus Castro <castrojesus39@gmail.com>
2025-02-13 21:12:47 -0800

    Created a separate file to handle all crud operations from and to the DB

---
+++ backend/DatabaseService.js
@@ -0,0 +1,19 @@
+import { DB } from "./connect";
+
+let createTable = `CREATE TABLE IF NOT EXISTS mice(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    weight REAL NOT NULL,
+    height REAL NOT NULL,
+    length REAL NOT NULL,
+    width REAL NOT NULL,
+    shape TEXT NOT NULL
+)`;
+
+DB.run(createTable, [], function(err) {
+    if (err) {
+        console.log("Error creating table: ", err.message);
+        return;
+    }
+    console.log("Table has been created!");
+});
