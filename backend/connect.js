284d4010b865c507976b844b55f745dc808235ca
Jesus Castro <castrojesus39@gmail.com>
2025-02-13 21:13:05 -0800

    moved items from connect to DatabaseService

--- backend/connect.js
+++ backend/connect.js
@@ -8,32 +8,12 @@ const DB = new sql3.Database(
     connected,
 );
 
-let createTable = `CREATE TABLE IF NOT EXISTS mice(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    weight REAL NOT NULL,
-    height REAL NOT NULL,
-    length REAL NOT NULL,
-    width REAL NOT NULL,
-    shape TEXT NOT NULL
-)`;
-
-
 function connected(err) {
     if (err) {
         console.error(err.message);
     }
     console.log("Connection successful");
 }
-
-
-DB.run(createTable, [], function(err) {
-    if (err) {
-        console.log("Error creating table: ", err.message);
-        return;
-    }
-    console.log("Table has been created!");
-});
 
 export { DB };
 
